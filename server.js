7a217a92a1649f1221aa259c3365951884541c73
filippinheiro <filippinheiro1@gmail.com>
2020-01-03 17:21:37 -0300

    env values

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const requireDir = require('require-dir')
 const cors = require('cors') 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 const app = express()
 app.use(express.json())
