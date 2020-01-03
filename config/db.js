e38a5fbc73be3cbfb2c4146eb35be86b243e6488
Filipe Pinheiro <filippinheiro1@gmail.com>
2020-01-03 17:37:49 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const connectionString = process.env.DATABASE_URL 
-console.log(connectionString)
 dotenv.config()
-
-
 
 const db = mongoose.connect(connectionString, {
     useNewUrlParser: true,
