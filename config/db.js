7b11df0e40916a61ea5fbddb1d6cdbbd8b7c7388
filippinheiro <filippinheiro1@gmail.com>
2020-01-03 16:26:25 -0300

    configuring Atlas DB deployment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
-const db = mongoose.connect('mongodb://localhost:27017/notableapi', {
-    useNewUrlParser: true
+
+const connectionString = 'mongodb+srv://filip:notable@notable-n7aon.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+const db = mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 module.exports = db
